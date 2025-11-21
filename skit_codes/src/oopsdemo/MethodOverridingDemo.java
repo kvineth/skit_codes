acc7f6a0de6d6a00ac3686feedc0ab356730e92d
Admin <Admin@DESKTOP-12I76GO>
2025-11-21 18:53:28 +0530

    First commit

---
+++ skit_codes/src/oopsdemo/MethodOverridingDemo.java
@@ -0,0 +1,39 @@
+package oopsdemo;
+
+class Payment //parent class
+{
+	void makePayment()
+	{
+		System.out.println("Processing payment using generic method");
+	}
+}
+
+class UpiPayment extends Payment //sub class
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using UPI");
+	}
+}
+class CardPayment extends Payment //sub class
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using credit card");
+	}
+}
+
+public class MethodOverridingDemo {
+
+	public static void main(String[] args) {
+		Payment payment;
+		
+		payment= new UpiPayment();
+		payment.makePayment();
+		
+		
+		payment=new CardPayment();
+		payment.makePayment();
+	}
+
+}
