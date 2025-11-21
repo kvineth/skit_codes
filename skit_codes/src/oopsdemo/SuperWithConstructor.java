f175e79c162fb9c6b2af9d0eef968a2085ddc1a1
Admin <Admin@DESKTOP-12I76GO>
2025-11-21 18:55:47 +0530

    First commit

---
+++ skit_codes/src/oopsdemo/SuperWithConstructor.java
@@ -0,0 +1,26 @@
+package oopsdemo;
+
+class User  //parent class
+{
+	User() //constructor
+	{
+		System.out.println("user account created");
+	}
+}
+
+class AdminUser extends User
+{
+	AdminUser()
+	{
+		super();
+		System.out.println("Admin privileges granted ");
+	}
+}
+public class SuperWithConstructor {
+
+	public static void main(String[] args) {
+		AdminUser admin=new AdminUser();
+
+	}
+
+}
