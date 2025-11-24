da8e4d2e08c523670724cae8d2bd4203addd64f0
Admin <Admin@DESKTOP-12I76GO>
2025-11-24 16:54:25 +0530

    First commit

---
+++ skit_codes/src/oopsdemo/MultiLevelDemo.java
@@ -0,0 +1,35 @@
+package oopsdemo;
+
+class Device
+{
+	void deviceType()
+	{
+		System.out.println("I am an electronic device");
+	}
+}
+
+class Phone extends Device
+{
+	void brand()
+	{
+		System.out.println("Brand : samsumng");
+	}
+}
+class SmartPhone extends Phone
+{
+	void feature() {
+		System.out.println("Feature : Touchscreen , camera , internet");
+	}
+}
+
+public class MultiLevelDemo {
+
+	public static void main(String[] args) {
+		SmartPhone obj=new SmartPhone ();
+		obj.deviceType();
+		obj.brand();
+		obj.feature();
+
+	}
+
+}
